13113e07c55547358c984a79d95bc7a938dd6fa9
Raman But-Husaim <raman_but-husaim@live.com>
2016-07-28 23:44:27 +0300

    [Functional programming using F#]: Chapter 02. Complete exercises 2.1 - 2.6

--- FPinFSharp/Chapter02/HOF.fs
+++ FPinFSharp/Chapter02/HOF.fs
@@ -6,3 +6,10 @@ let g = fun x -> x * x
 let h = f << g
 
 let weight ro = fun s -> ro * s ** 3.0
+
+let waterWeight = weight 1000.0
+
+// preferred way of higher order function delcaration
+let weightPreferred ro s = ro * s ** 3.0
+
+let someWeight = weightPreferred 3000.0
--- FPinFSharp/Chapter02/Program.fs
+++ FPinFSharp/Chapter02/Program.fs
@@ -3,8 +3,37 @@
 
 module Main
 
+// exercise 2.1
+let f n = (n % 2 = 0 || n % 3 = 0 ) && (n % 5 <> 0)
+
+// exercise 2.2
+let rec pow (str: string) n =
+    match n with
+    | 0 -> ""
+    | t -> str + pow str (n - 1)
+
+// exercise 2.3
+let isIthChar (str: string) i ch =
+    match i with
+    | t when i < 0 -> false
+    | t when i >= (String.length str) -> false
+    | t -> str.[t] = ch
+
+// exercise 2.4
+let occFromIth (str: string) i ch =
+    match i with
+    | t when t < 0 -> 0
+    | t when t >= (String.length str) -> 0
+    | t -> Seq.skip t str |> Seq.filter (fun c -> c = ch) |> Seq.length
+
+// exercise 2.5
+let occInString (str: string) ch =
+    Seq.filter (fun c -> c = ch) str |> Seq.length
+
+// exercise 2.6
+let notDivisible d n = n % d <> 0
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     0 // return an integer exit code
-

5383bd76f7a5d1928d281474197c4df7bc8f18c8
Raman But-Husaim <raman_but-husaim@live.com>
2016-07-31 16:42:02 +0300

    [Functional programming using F#]: Chapter 03. Quadratic equations

---
+++ FPinFSharp/Chapter03/QuadraticEquations.fs
@@ -0,0 +1,13 @@
+﻿module QuadraticEquations
+
+type Equation = float * float * float
+
+type Solution = float * float
+
+let solve (a, b, c) = 
+    let sqrtD =
+        let d = b * b - 4.0 * a * c
+        if d < 0.0 || a = 0.0
+        then failwith "discriminant is negative or a=0.0"
+        else sqrt d
+    ((-b + sqrtD) / (2.0 * a), (-b - sqrtD) / (2.0 * a))

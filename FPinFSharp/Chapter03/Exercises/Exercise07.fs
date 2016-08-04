c77cb985b970a823ae49f9dd7fae4e99b3d69c2a
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-04 22:39:00 +0300

    [Functional programming using F#]: Chapter 03. Exercise 07

---
+++ FPinFSharp/Chapter03/Exercises/Exercise07.fs
@@ -0,0 +1,21 @@
+﻿module Exercises.Exercise07
+
+type Shape = 
+    | Circle of float
+    | Square of float
+    | Triangle of float * float * float
+
+let isShape shape = 
+    match shape with
+    | Circle r -> r > 0.0
+    | Square a -> a > 0.0
+    | Triangle(a, b, c) -> a > 0.0 && b > 0.0 && c > 0.0 && a < b + c && b < c + a && c < a + b
+
+let area x = 
+    match x with
+    | _ when not (isShape x) -> None
+    | Circle r -> Some(System.Math.PI * r * r)
+    | Square a -> Some(a * a)
+    | Triangle(a, b, c) -> 
+        let s = (a + b + c) / 2.0
+        Some(sqrt (s * (s - a) * (s - b) * (s - c)))

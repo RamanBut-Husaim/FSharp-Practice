d5604b1d011ae1793e12266969c3de27d62ae86e
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-04 22:28:26 +0300

    [Functional programming using F#]: Chapter 03. Exercise 05

---
+++ FPinFSharp/Chapter03/Exercises/Exercise05.fs
@@ -0,0 +1,15 @@
+﻿module Exercises.Exercise05
+
+type Solution = 
+    | TwoRootes of float * float
+    | OneRoot of float
+    | NoRoot
+
+let solve (a, b, c) = 
+    let d = b * b - 4.0 * a * c
+    match d with
+    | t when d < 0.0 || a = 0.0 -> NoRoot
+    | 0.0 -> OneRoot((-b + (sqrt d)) / (2.0 * a))
+    | t -> 
+        let sqrtD = sqrt d
+        TwoRootes((-b + sqrtD) / (2.0 * a), (-b - sqrtD) / (2.0 * a))

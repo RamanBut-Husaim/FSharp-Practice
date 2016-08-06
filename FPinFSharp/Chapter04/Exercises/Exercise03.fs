b74d0212b453f67de16a4355611f01783de7bfc0
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-06 15:31:52 +0300

    [Functional programming using F#]: Chapter 04. Exercise 03

---
+++ FPinFSharp/Chapter04/Exercises/Exercise03.fs
@@ -0,0 +1,13 @@
+﻿module Chapter04.Exercises.Exercise03
+
+let isEven n = n % 2 = 0
+
+let rec evenGenerator number counter = 
+    match number with
+    | n when counter <= 0 -> []
+    | n when isEven n -> 
+        let rest = evenGenerator (number + 1) (counter - 1)
+        (number :: rest)
+    | n -> evenGenerator (number + 1) counter
+
+let evenN n = evenGenerator 0 n

100bddbdf4913692db2368ca96167f54ea66f1dd
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-06 15:44:48 +0300

    [Functional programming using F#]: Chapter 04. Exercise 04

---
+++ FPinFSharp/Chapter04/Exercises/Exercise04.fs
@@ -0,0 +1,8 @@
+﻿module Chapter04.Exercises.Exercise04
+
+let rec altSumInternal elements signum = 
+    match elements with
+    | [] -> 0
+    | x :: xs -> (x * signum) + altSumInternal xs (-signum)
+
+let altsum elements = altSumInternal elements 1

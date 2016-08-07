c4733360e06ec3205151d3fc5ce38ef1fd9b13df
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-07 17:30:50 +0300

    [Functional programming using F#]: Chapter 04. Exercise 05

---
+++ FPinFSharp/Chapter04/Exercises/Exercise05.fs
@@ -0,0 +1,13 @@
+﻿module Chapter04.Exercises.Exercise05
+
+let isOdd number = number % 2 <> 0
+
+let rec rmoddInternal index elements =
+    match elements with
+    | [] -> []
+    | x :: xs -> 
+        let rest = rmoddInternal (index + 1) xs
+        if isOdd x then rest
+        else x :: rest
+
+let rmodd elements = rmoddInternal 0 elements

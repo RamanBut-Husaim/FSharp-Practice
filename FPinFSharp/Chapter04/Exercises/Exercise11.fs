a794942799c77b568699feeb400a0c065c9a2b47
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-07 20:35:18 +0300

    [Functional programming using F#]: Chapter 04. Exercise 11 - count, insert

---
+++ FPinFSharp/Chapter04/Exercises/Exercise11.fs
@@ -0,0 +1,27 @@
+﻿module Chapter04.Exercises.Exercise11
+
+let rec isWeaklyAscending elements = 
+    match elements with
+    | [] -> true
+    | [ x ] -> true
+    | x1 :: x2 :: xs -> 
+        let other = isWeaklyAscending xs
+        (x1 <= x2) && other
+
+let rec count pair = 
+    match pair with
+    | ([], x) -> 0
+    | (x :: xs, y) when x > y -> 0
+    | (x :: xs, y) -> 
+        let rest = count (xs, y)
+        if (x = y) then (rest + 1)
+        else rest
+
+let rec insert elements value = 
+    match elements with
+    | [] -> [ value ]
+    | x :: xs when x <= value -> 
+        let rest = insert xs value
+        x :: rest
+    | x :: xs when x > value -> value :: x :: xs
+    | xs -> xs

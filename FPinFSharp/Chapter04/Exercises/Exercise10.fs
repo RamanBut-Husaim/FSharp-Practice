d6eb0c05c0bfad0830758d12e7c9f2798d700404
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-07 19:15:08 +0300

    [Functional programming using F#]: Chapter 04. Exercise 10

---
+++ FPinFSharp/Chapter04/Exercises/Exercise10.fs
@@ -0,0 +1,10 @@
+﻿module Chapter04.Exercises.Exercise10
+
+let rec prefix elements = 
+    match elements with
+    | ([], []) -> true
+    | (x :: xs, y :: ys) -> 
+        let rest = prefix (xs, ys)
+        (x = y) && rest
+    | ([], _) -> true
+    | (_, _) -> false

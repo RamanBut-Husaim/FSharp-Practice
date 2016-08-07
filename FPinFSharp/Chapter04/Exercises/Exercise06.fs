c113adff8dd998effb56ee12482ae23f3fc8dc41
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-07 17:55:22 +0300

    [Functional programming using F#]: Chapter 04. Exercise 06

---
+++ FPinFSharp/Chapter04/Exercises/Exercise06.fs
@@ -0,0 +1,11 @@
+﻿module Chapter04.Exercises.Exercise06
+
+let isEven number = number % 2 = 0
+
+let rec rmeven elements = 
+    match elements with
+    | [] -> []
+    | x :: xs -> 
+        let rest = rmeven xs
+        if isEven x then rest
+        else x :: rest

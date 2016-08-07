957a2afdc888292d74a7d7cbb3eec84892654bea
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-07 18:57:28 +0300

    [Functional programming using F#]: Chapter 04. Exercise 09

--- FPinFSharp/Chapter04/Exercises/Exercise08.fs
+++ FPinFSharp/Chapter04/Exercises/Exercise08.fs
@@ -1,3 +1,11 @@
 ﻿module Chapter04.Exercises.Exercise08
 
+let rec splitInternal elements switch = 
+    match elements with
+    | [] -> ([], [])
+    | x :: xs -> 
+        let (odd, even) = splitInternal xs (not switch)
+        if switch then (x :: odd, even)
+        else (odd, x :: even)
 
+let split elements = splitInternal elements true

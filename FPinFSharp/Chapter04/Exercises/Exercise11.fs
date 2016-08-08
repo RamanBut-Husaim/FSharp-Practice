1027dd65fe5889e5fbc0ac0bd0123add5bf28acd
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-08 21:43:57 +0300

    [Functional programming using F#]: Chapter 04. Exercise 11 - minus

--- FPinFSharp/Chapter04/Exercises/Exercise11.fs
+++ FPinFSharp/Chapter04/Exercises/Exercise11.fs
@@ -42,3 +42,36 @@ let rec intersect elements =
         else 
             let rest = intersect (x2 :: xs, y2 :: ys)
             x1 :: rest
+
+let plus elements = 
+    let (firstList, secondList) = elements
+    
+    let rec plusInternal source result = 
+        match source with
+        | [] -> result
+        | x :: xs -> 
+            let insertResult = insert result x
+            plusInternal xs insertResult
+    plusInternal secondList firstList
+
+let minus elements = 
+    let (firstList, secondList) = elements
+    
+    let remove element listToScan = 
+        let rec removeInternal element listToScan removedElements = 
+            match listToScan with
+            | [] -> removedElements
+            | x :: xs when x = element -> xs
+            | x :: xs -> removeInternal element xs (removedElements @ [ x ])
+        removeInternal element listToScan []
+    
+    let rec minusInternal original itemsToScan = 
+        match original with
+        | [] -> []
+        | x :: xs -> 
+            let clearedItemsToScan = remove x itemsToScan
+            let rest = minusInternal xs clearedItemsToScan
+            if List.length itemsToScan <> List.length clearedItemsToScan then rest
+            else x :: rest
+    
+    minusInternal firstList secondList

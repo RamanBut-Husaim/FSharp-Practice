2755abee3c08d09c4a8e2868248245f476dac61f
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-10 23:11:18 +0300

    [Functional programming using F#]: Chapter 04. Exercise 13. findMinimal with tests

---
+++ FPinFSharp/Chapter04.Tests/Exercises/Exercise13.fs
@@ -0,0 +1,22 @@
+﻿module Chapter04.Tests.Exercises.Exercise13
+
+    open Xunit
+    open FsUnit.Xunit
+
+    open Chapter04.Exercises.Exercise13
+
+    [<Fact>]
+    let ``findSmallest - when the list is empty - int.MaxValue is returned`` () =
+        findSmallest [] |> should equal System.Int32.MaxValue
+    
+    [<Fact>]
+    let ``findSmallest - when the list contains positive values - the smallest value is returned`` () =
+        findSmallest [1; 3; 4; 1; 3; 5; 13; 22] |> should equal 1
+    
+    [<Fact>]
+    let ``findSmallest - when the list contains negative values - the smallest value is returned`` () =
+        findSmallest [-1; -4; -122; -57; -91; -281] |> should equal -281
+
+    [<Fact>]
+    let ``findSmallest - when the list contains positive and negative values - the smallest value is returned`` () =
+        findSmallest [-34; 11; 34; 182; -918; 741; 334; 123] |> should equal -918
---
+++ FPinFSharp/Chapter04/Exercises/Exercise13.fs
@@ -0,0 +1,11 @@
+﻿module Chapter04.Exercises.Exercise13
+
+let findSmallest elements =
+    let rec findInternal minElement els =
+        match els with
+        | [] -> minElement
+        | x :: xs ->
+            if x < minElement then findInternal x xs
+            else findInternal minElement xs
+    
+    findInternal System.Int32.MaxValue elements

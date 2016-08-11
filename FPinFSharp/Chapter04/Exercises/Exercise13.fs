c5fcf260485ad335eb9082a35b19fc9cfb09a5f2
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-11 22:16:33 +0300

    [Functional programming using F#]: Chapter 04. Exercise 13. Completed

--- FPinFSharp/Chapter04/Exercises/Exercise13.fs
+++ FPinFSharp/Chapter04/Exercises/Exercise13.fs
@@ -1,11 +1,28 @@
 ﻿module Chapter04.Exercises.Exercise13
 
-let findSmallest elements =
-    let rec findInternal minElement els =
+let findSmallest elements = 
+    let rec findInternal minElement els = 
         match els with
         | [] -> minElement
-        | x :: xs ->
+        | x :: xs -> 
             if x < minElement then findInternal x xs
             else findInternal minElement xs
-    
     findInternal System.Int32.MaxValue elements
+
+let rec delete (value, elements) = 
+    match elements with
+    | [] -> []
+    | x :: xs -> 
+        if x = value then xs
+        else 
+            let rest = delete (value, xs)
+            x :: rest
+
+let rec bruteSort elements =
+    match elements with
+    | [] -> []
+    | xs ->
+        let smallest = findSmallest xs
+        let rest = delete (smallest, xs)
+        let sorted = bruteSort rest
+        smallest :: sorted

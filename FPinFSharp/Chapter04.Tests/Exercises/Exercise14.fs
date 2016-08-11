f28e55a225faa4ef4e1716b1152b1227570e7b8d
Raman But-Husaim <raman_but-husaim@live.com>
2016-08-11 22:31:35 +0300

    [Functional programming using F#]: Chapter 04. Exercise 14

---
+++ FPinFSharp/Chapter04.Tests/Exercises/Exercise14.fs
@@ -0,0 +1,14 @@
+﻿module Chapter04.Tests.Exercises.Exercise14
+
+    open Xunit
+    open FsUnit.Xunit
+
+    open Chapter04.Exercises.Exercise14
+
+    [<Fact>]
+    let ``findSmallest - when the list is empty - None is returned`` () =
+        findSmallest [] |> should equal None
+    
+    [<Fact>]
+    let ``findSmallest - when the list contains elements - the smallest is returned`` () =
+        findSmallest [-14; 2; 4; 18; -88] |> should equal (Some -88)
---
+++ FPinFSharp/Chapter04/Exercises/Exercise14.fs
@@ -0,0 +1,11 @@
+﻿module Chapter04.Exercises.Exercise14
+
+let findSmallest elements = 
+    let rec findInternal minElement els = 
+        match els with
+        | [] -> Some minElement
+        | x :: xs -> 
+            if x < minElement then findInternal x xs
+            else findInternal minElement xs
+    if elements = [] then None
+    else findInternal System.Int32.MaxValue elements

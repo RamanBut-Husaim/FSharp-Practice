7a51d0caa0e745e83d677dfc75539a323e89aee5
Raman But-Husaim <raman_but-husaim@live.com>
2017-01-02 23:35:44 +0300

    FPinFSharp: ch5 ex1 add unit-tests +semver:minor'

--- FPinFSharp/src/test/Chapter05.Tests/Exercises/Exercise01.fs
+++ FPinFSharp/src/test/Chapter05.Tests/Exercises/Exercise01.fs
@@ -4,6 +4,21 @@ module Chapter05.Tests.Exercises.Exercise01
     open FsUnit.Xunit
 
     open Chapter05.Exercises.Exercise01
-//     [<Fact>]
-//     let ``filter - When The List Is Empty - Should Return An Empty Collection`` () =
-//         filter []: int list (fn x -> true) |> should be empty
+
+    [<Fact>]
+    let ``filter - When The List Is Empty - Should Return An Empty Collection`` () =
+        filter ([]: int list) (fun x -> true) |> should be Empty
+
+    [<Fact>]
+    let ``filter - When The List Is Not Empty - Should Return A Correct Result`` () =
+        // arrange
+        let elements = [4; 1; 5; 2; 7; 3]
+        let filterFunc = fun x -> x % 2 = 0
+        let expected = List.filter filterFunc elements
+
+        // act
+        let actual = filter elements filterFunc
+
+        // assert
+        actual |> should equal expected
+        ()
